93e04d23ae7811142b0d081c1987beb3408bc368
Alex Losser <lossalex@isu.edu>
2021-04-01 12:29:23 -0700

    Updated .gitignore to include underscore files and created a modules-info file for project with javafx implemented.

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module TodoListManager.app.main {
+    requires javafx.controls;
+    requires com.google.common;
+    requires jdk.jshell;
+    requires com.google.gson;
+    exports edu.isu.cs.cs2263.todoListManager;
+}
